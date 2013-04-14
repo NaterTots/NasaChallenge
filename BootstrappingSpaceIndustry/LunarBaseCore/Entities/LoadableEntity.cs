c164ff856c8fe637edb6fb818b3462f256297f3c
Ryan Wilson <link1305@gmail.com>
2013-04-13 20:02:10 -0400

    Added EntityManagerBase and LoadableEntity classes
    These will be used to load things from XML.  Instead of "things", i'm
    calling anything that can be loaded from xml an "entity"

---
+++ BootstrappingSpaceIndustry/LunarBaseCore/Entities/LoadableEntity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore.Entities
+{
+	/// <summary>
+	///  Derive from this class for any objects that will be loaded from XML
+	/// </summary>
+	public class LoadableEntity : IEquatable<LoadableEntity>
+	{
+		public LoadableEntity()
+		{
+			ID = Guid.NewGuid();
+		}
+
+		/// <summary>
+		/// This is the name of the object as specified in the XML; this could be referenced by other objects in the XML, and should not contain spaces.
+		/// </summary>
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public Guid ID
+		{
+			get;
+			private set;
+		}
+
+		#region IEquatable<LoadableObject> Members
+
+		public bool Equals(LoadableEntity other)
+		{
+			return ID.Equals(other.ID);
+		}
+
+		#endregion
+	}
+}
