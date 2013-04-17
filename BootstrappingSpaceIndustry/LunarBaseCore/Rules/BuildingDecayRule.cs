33cff2d505e71080ffb3fee81fdcc49aa218c73e
Jeramiah Cieszynski <jcieszynski@gmail.com>
2013-04-16 23:56:13 -0400

    Rules simple skeleton code

---
+++ BootstrappingSpaceIndustry/LunarBaseCore/Rules/BuildingDecayRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore.Rules
+{
+    /// <summary>
+    /// This rule defines the decay algorithm for items of type BuildingItemType.
+    /// </summary>
+    public class BuildingDecayRule : RuleBase
+    {
+    }
+}
---
+++ BootstrappingSpaceIndustry/LunarBaseCore/Rules/RuleBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore.Rules
+{
+    /// <summary>
+    /// Base class for all Rules.
+    /// </summary>
+    public class RuleBase
+    {
+    }
+}
