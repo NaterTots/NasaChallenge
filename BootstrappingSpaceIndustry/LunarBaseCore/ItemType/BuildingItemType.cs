1ed44c63266b026f726f1320846a4894be8b04d7
Jeramiah Cieszynski <jcieszynski@gmail.com>
2013-04-16 23:03:16 -0400

    Checking in skeleton code for Item and ItemType

---
+++ BootstrappingSpaceIndustry/LunarBaseCore/ItemType/BuildingItemType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore.ItemType
+{
+    public class BuildingItemType : ItemTypeBase
+    {
+    }
+}
---
+++ BootstrappingSpaceIndustry/LunarBaseCore/ItemType/ItemTypeBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore.ItemType
+{
+    /// <summary>
+    /// Base class for all ItemType objects.  Defines all standard methods of an ItemType.
+    /// </summary>
+    public class ItemTypeBase //??? should this be abstract or an interface?
+    {
+        public ItemTypeBase()
+        {
+        }
+
+    }
+}
