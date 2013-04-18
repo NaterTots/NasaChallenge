5276f8d2c4c429c37fbda58e137ef1d5da261770
Dan V <stelth20@gmail.com>
2013-04-17 23:13:51 -0400

    GameEngine
    Contains logic to change the window, and keep track of the time that
    passed then call the structure engine's update

---
+++ BootstrappingSpaceIndustry/LunarBaseCore/GameEngine.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBaseCore
+{
+    public enum gameState { mainMenu, playing, winScreen, loseScreen };
+
+
+    public class GameEngine
+    {
+        LunarTime timeEngine;
+
+        long totalTime;
+
+        int intervals;
+
+        public gameState currentState
+        {
+            get;
+            set;
+        }
+
+        //StructureEngine structs;
+        public EventHandler timerTicks;
+
+        static GameEngine()
+        {
+
+            //timerTicks.AddHandler(LunarTime.TimeTick, new RoutedEventHandler(OnTimerTick));
+        }
+
+        //this is the game loop
+        public void RunGame()
+        {
+            totalTime = 0;
+            timeEngine = new LunarTime();
+            currentState = gameState.mainMenu;
+
+            while (currentState != gameState.winScreen || currentState != gameState.loseScreen)
+            {
+                if (currentState == gameState.mainMenu)
+                {
+                    //mainWindow = new mainScreen();
+                }
+
+                else if (currentState == gameState.playing)
+                {
+                    //mainWindow = new PlayingScreen();
+                }
+
+                else if (currentState == gameState.loseScreen)
+                {
+                    //mainWindow = new LoseScreen();
+                }
+
+                else if (currentState == gameState.winScreen)
+                {
+                    //mainWindow = new WinScreen();
+                }
+            }
+
+        }
+
+        public void setWin()
+        {
+            currentState = gameState.winScreen;
+
+            //mainWindow = new WinScreen();
+        }
+
+        public void setLose()
+        {
+            currentState = gameState.loseScreen;
+            //mainWindow = new LoseScreen();
+        }
+
+        public void TurnStart()
+        {
+            timeEngine.Start();
+        }
+
+        private void OnTimerTick()
+        {
+            totalTime += 1;
+
+            if (totalTime >= intervals)
+            {
+                timeEngine.Pause();
+
+                //structEngine.update(totalTime);
+
+                totalTime = 0;
+            }
+        }
+    }
+}
