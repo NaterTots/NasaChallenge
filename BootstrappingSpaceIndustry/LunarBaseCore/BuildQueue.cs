c0a3ab270541cf54d6e4b88e0491625c5d781da6
Nate <hodge.nate@gmail.com>
2013-04-18 23:08:04 -0400

    Created Build Queue

--- BootstrappingSpaceIndustry/LunarBaseCore/BuildQueue.cs
+++ BootstrappingSpaceIndustry/LunarBaseCore/BuildQueue.cs
@@ -5,7 +5,46 @@ using System.Text;
 
 namespace LunarBaseCore
 {
-    class BuildQueue
+    public class BuildQueue
     {
+        Queue<WorkItem> _queue = new Queue<WorkItem>();
+
+        public void AddWorkItem(WorkItem item)
+        {
+            _queue.Enqueue(item);
+        }
+
+        public WorkItem GetNextWorkItem()
+        {
+            return _queue.Dequeue();
+        }
+
+        public WorkItem PeekAtNextWorkItem()
+        {
+            return _queue.Peek();
+        }
+
+        public IEnumerable<WorkItem> QueueEnumerable()
+        {
+            foreach (WorkItem item in _queue)
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public class WorkItem
+    {
+        //Anything necessary here?
+    }
+
+    public class ConstructionWorkItem : WorkItem
+    {
+        public ConstructionWorkItem(ItemTypeBase item)
+        {
+            ItemType = item;
+        }
+
+        public ItemTypeBase ItemType { get; private set; }
     }
 }
