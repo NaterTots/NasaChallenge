2fb37812b9c14f864ae48c03c549d9fdb1a2c7b7
Ryan Wilson <link1305@gmail.com>
2013-04-18 23:50:13 -0400

    Added controls for schematics and buildqueue

---
+++ BootstrappingSpaceIndustry/LunarBase.WPF/ViewModels/BuildQueueViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LunarBase.WPF.ViewModels
+{
+	internal class BuildQueueViewModel : ViewModelBase
+	{
+		// Use ServiceManager to access data from the simulation
+	}
+}
